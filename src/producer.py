dcf8877e475757a9aa6e2269488d9e6a8c5668b4
Michal Cialowicz <cialowicz@gmail.com>
2014-07-22 17:39:28 -0700

    Updating comment

--- src/producer.py
+++ src/producer.py
@@ -4,9 +4,9 @@ from boto.s3.connection import S3Connection
 from boto.sqs.message import Message
 import boto.sqs
 
-# Reads all the .log files in an S3 bucket,
-# locates exceptions within the log file,
-# and writes basic exception information to an SQS queue
+# Should read all the .log files in an S3 bucket,
+# parse each log file to locate code exceptions,
+# and then write basic exception information to an SQS queue.
 
 # AWS Keys
 AWS_key    = ''
